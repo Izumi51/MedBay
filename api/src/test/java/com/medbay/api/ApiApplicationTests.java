633349d1c3341ed1667d02945ddc3a1b8b97ebb8
Izumi51 <joaoizumi27@gmail.com>
2025-11-02 17:31:17 -0300

    init

---
+++ api/src/test/java/com/medbay/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medbay.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
